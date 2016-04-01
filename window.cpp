9a00dfedb9a00a021af42ef399332b7ed2c11112
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-01 11:40:11 +0100

    Update window.cpp

--- window.cpp
+++ window.cpp
@@ -7,13 +7,13 @@
 Window::Window() : func(0),count(0)
 {
 	
-	knob = new QwtKnob;
+	//knob = new QwtKnob;
 	// set up the gain knob 
-	knob->setValue(0);
+	//knob->setValue(0);
 
 	// use the Qt signals/slots framework to update the gain -
 	// every time the knob is moved, the setFunc function will be called
-	connect( knob, SIGNAL(valueChanged(double)), SLOT(setFunc(double)) );
+	//connect( knob, SIGNAL(valueChanged(double)), SLOT(setFunc(double)) );
 
 	// set up the thermometer
         //thr=new QPushButton;
@@ -32,30 +32,41 @@ Window::Window() : func(0),count(0)
 		xData[index] = index;
 		yData[index] = 0;
 	}
+	for( int index=0; index<plotDataSize2; ++index )
+	{
+		xData2[index] = index;
+		yData2[index] = 0;
+	}
 
-	curve = new QwtPlotCurve;
-	plot = new QwtPlot;
 	// make a plot curve from the data and attach it to the plot
 	curve->setSamples(xData, yData, plotDataSize);
 	curve->attach(plot);
 
 	plot->replot();
 	plot->show();
+	
+	curve2->setSamples(xData2, yData2, plotDataSize2);
+	curve2->attach(plot2);
+
+	plot2->replot();
+	plot2->show();
 
 
 	// set up the layout - knob above thermometer
 	vLayout = new QVBoxLayout;
-	vLayout->addWidget(knob);
+	//vLayout->addWidget(knob);
 //	vLayout->addWidget(m_label);
 //	vLayout->addWidget(m1_label);
 //	vLayout->addWidget(thermo);
 
 	// plot to the left of knob and thermometer
-	hLayout = new QHBoxLayout;
-	hLayout->addLayout(vLayout);
-	hLayout->addWidget(plot);
+	//hLayout = new QHBoxLayout;
+	//hLayout->addLayout(vLayout);
+	vLayout->addWidget(plot);
+	vLayout->addWidget(plot2);
 
-	setLayout(hLayout);
+
+	setLayout(vLayout);
 
 	// This is a demo for a thread which can be
 	// used to read from the ADC asynchronously.
@@ -83,10 +94,16 @@ void Window::timerEvent( QTimerEvent * )
 		inval=adcreader->get_samples();
 		value=(double) inval;
 		// add the new input to the plot
+		
 		memmove( yData, yData+1, (plotDataSize-1) * sizeof(double) );
 		yData[plotDataSize-1] = value;
 		curve->setSamples(xData, yData, plotDataSize);
 		plot->replot();
+		
+		memmove( yData2, yData2+1, (plotDataSize2-1) * sizeof(double) );
+		yData[plotDataSize2-1] = value;
+		curve2->setSamples(xData2, yData2, plotDataSize2);
+		plot2->replot();
 	}
 	// set the thermometer value
 	//thermo->setValue( inVal + 20 );
