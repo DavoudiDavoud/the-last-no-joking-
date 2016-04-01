223d01227499ba132986def7ae6198dd4cdb246b
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-01 11:13:49 +0100

    Update window.h

--- window.h
+++ window.h
@@ -35,16 +35,25 @@ private:
 //	QwtThermo    *thermo;
 	QwtPlot      *plot;
 	QwtPlotCurve *curve;
+	
+	QwtPlot      *plot2;
+	QwtPlotCurve *curve2;
 
 	// layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
 	QVBoxLayout  *vLayout;  // vertical layout
 	QHBoxLayout  *hLayout;  // horizontal layout
 
 	static const int plotDataSize = 1000;
+	static const int plotDataSize2 = 1000;
+
 
 	// data arrays for the plot
 	double xData[plotDataSize];
 	double yData[plotDataSize];
+	
+	double xData2[plotDataSize2];
+	double yData2[plotDataSize2];
+	
 
         double func;
 	int count;
